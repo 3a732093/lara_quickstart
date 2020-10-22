7a2082d43fbaa2b2fe0dd75a13c8032e9c2d650d
廖韋臣 <rax02270521@gmail.com>
2020-10-22 16:16:10 +0800

    初步建置三個路由

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+
+Route::group(['middleware' => 'web'], function () {
+
+
+    Route::get('/', function () {
+
+    });
+
+
+    Route::post('/task', function (Request $request) {
+
+    });
+
+
+    Route::delete('/task/{task}', function (Task $task) {
+
+    });
 });
